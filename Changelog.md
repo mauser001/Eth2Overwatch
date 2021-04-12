64a260e61c6f697431b51c1693a12bec1bf4f9ba
mauser001 <mauser001@hotmail.com>
2021-04-12 21:15:56 +0200

    Changed eth1 flag --rpc to --html

--- Changelog.md
+++ Changelog.md
@@ -1,5 +1,8 @@
 # Eth2Overwatch - Changelog
 
+#### Version 1.0.7-RC.4
++ Changed eth1 flag --rpc to --html
+
 #### Version 1.0.7-RC.3
 + Refactoring: Splitted ProcessController into abstract base and specific controllers 
 
--- Controllers/Eth1Controller.cs
+++ Controllers/Eth1Controller.cs
@@ -48,7 +48,7 @@ namespace Eth2Overwatch.Controllers
             this.fileName = "cmd.exe";
             this.directory = this.executablePath;
             this.commands = new string[1];
-            var ipc = useLocalEth1Node ? " --ipcpath=http://127.0.0.1:8545/ --rpc" : "";
+            var ipc = useLocalEth1Node ? " --ipcpath=http://127.0.0.1:8545/ --http" : "";
             this.commands[0] = String.Format(@"geth --datadir=" + this.dataDir + ipc + goerli + add);
         }
 
